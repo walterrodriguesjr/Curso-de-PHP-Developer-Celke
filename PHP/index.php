f557ba581a22584ab99a2f91d964d21dbe6a6f7a
Walter <walterrjr.86@gmail.com>
2022-05-24 20:41:26 -0300

    24-05-22/2

--- PHP/index.php
+++ PHP/index.php
@@ -1,5 +1,44 @@
 <?php 
 
+/* 5 */
 echo "teste curso PHP Celke";
 
-?>
+echo "<br>";
+
+/* 6 */
+$idade = 22;
+$nome = "Walter";
+echo "Meu nome é " . $nome . " e tenho " . $idade . " anos de idade.";
+
+echo "<br>";
+
+/* 7 */
+$result = "2"; 
+var_dump($result);
+echo "<br>";
+$result_dois = $result +1;
+var_dump($result_dois);
+echo "<br>";
+$result_tres = (string)$result_dois;
+var_dump($result_tres);
+echo "<br>";
+var_dump($result_quatro = (float)$result_tres);
+
+echo "<br>";
+
+/* 8 */
+$x = 10;
+$y = 5;
+$result_soma = $x + $y;
+echo "O resultado da soma é: " . $result_soma;
+
+echo "<br>";
+
+/* 9 */
+$a = 1;
+$b = 2;
+$c = 3;
+$result_atribuicao = 6;
+echo "Somar o valor $result_atribuicao pelo valor $a <br>";
+$result_atribuicao += $a;
+echo "Resultado da adição: $result_atribuicao <br>";
