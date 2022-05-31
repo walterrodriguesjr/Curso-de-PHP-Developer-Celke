93aa3b3cead024cd3c9ae1cd33baba675ac7ba50
Walter <you@example.com>
2022-05-31 13:10:17 -0300

    31-05-22

--- PHP/index.php
+++ PHP/index.php
@@ -71,16 +71,67 @@ echo "<br>";
 
 /* 13 */
 $numf = 11;
-if($numf == 10){
+if ($numf == 10) {
     echo "é igual a 10<br>";
-}else{
+} else {
     echo "não é igual a 10<br>";
 };
 
 /* 14 */
 $numf = 15;
-if($numf == 11){
+if ($numf == 11) {
     echo "é igual a 11<br>";
-}else{
+} else {
     echo "não é igual a 11<br>";
 };
+
+/* 15 */
+$numg = 20;
+
+if ($numg == 10) {
+    echo "$numg é igual a 10<br>";
+} else if ($numg == 15) {
+    echo "$numg é igual a 15<br>";
+} else {
+    echo "nenhuma igualdade<br>";
+}
+
+/* 16 */
+$numh = 22;
+switch ($numh) {
+    case 1:
+        echo "número 1<br>";
+        break;
+    case 22:
+        echo "número 22<br>";
+        break;
+    default:
+        echo "nenhum número<br>";
+        break;
+}
+
+/* 17 */
+$numI = 0;
+while ($numI <= 10) {
+    echo $numI . " usuário<br>";
+    $numI++;
+};
+
+/* 18 */
+$numJ = 1;
+do {
+    echo $numJ . " repetindo<br>";
+    $numJ++;
+} while ($numJ <= 1);
+
+/* 19 */
+$numK = 0;
+for ($numK = 0; $numK < 10; $numK++) {
+    echo "Cliente número: " . $numK . "<br>";
+}
+
+/* 20 */
+$alunos = ["A", "B", "C", "D"];
+foreach ($alunos as $aluno) {
+    echo "Nome: " . $aluno . "<br>";
+}
